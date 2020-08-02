9209cac0fb8e4a80d6b59dbc356886d7474cc6b7
yaowenqiang <yaowenqiang111@163.com>
2020-08-02 16:57:37 +0800

    add complex multi-service demo

---
+++ complex/client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Im some other Page
+            <link to="/">Go back home</link>
+        </div>
+    );
+};
+
